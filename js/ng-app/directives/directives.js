80a8dc98db0401d43575b175394c9321627d801a
enlightenone <stevenwchow@gmail.com>
2016-01-01 11:51:06 -0800

    Reorganizing the codes

--- js/ng-app/directives/directives.js
+++ js/ng-app/directives/directives.js
@@ -1,4 +1,6 @@
+// Angular Directives
 app.directive('gameTemplate', function($location){
+  //This is reusable directive to render template for each section on main index.html page
    return {
       restrict: 'E',
       templateUrl: function(elem, attr){
